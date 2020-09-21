236f7b3960aa4827d0c8316a66d35cb4d3ba9bd9
Nikita Manichev <nikita@whatashot.app>
2020-09-21 16:31:46 +0300

    Removed test from solution.

--- test/type-helper.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-const AsyncFunction = (async () => {}).constructor;
-
-module.exports.isFunction = (func) => func instanceof Function;
-
-module.exports.isAsyncFunction = (func) => module.exports.isFunction(func) && func instanceof AsyncFunction;
